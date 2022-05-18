33b6bf714836d3512fa62e933cdef0715d2d91c2
RubaALmohya <ruba12399@gmail.com>
2022-05-18 14:32:46 +0300

    added

--- Recursion_Lambda.py
+++ Recursion_Lambda.py
@@ -1,32 +1,14 @@
-# Python3 program to count vowels
-# in a string
- 
-# Function to check the Vowel
-import re
+def VowelCount(s):
+    vowels = "aeiouAEIOU"
+    if s == "":
+        return 0
+    elif s[0] in vowels:
+        return 1 + VowelCount(s[1:])
+    else:
+        return 0 + VowelCount(s[1:])
 
+print(VowelCount("I love pythON"))
 
-def isVowel(check):
-    return check.upper()  in ['A', 'E', 'I', 'O', 'U'] 
-
-def isVowel2(check2):
-    return check2.lower() in ['a','e','i','o','u']
-
-# Returns count of vowels in str
-def countVowels(str):
-    count = 0
-    for i in range(len(str)):
- 
-        # Check for vowel
-        if isVowel(str[i]):
-         
-            count += 1
-        elif isVowel2(str[i]):
-            count += 1
-    print("total vowels number is: ")
-    return count
-
-str = input("write :")
-print(countVowels(str))
 print("____________Q2_____________")
 result=list(map(lambda n: n * n, [40, 35, 10, 15, 20]))
 print(result)
