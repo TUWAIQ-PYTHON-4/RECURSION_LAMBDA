9c5f57f05d9e11f0f3ba1335bd83ac4f20feacb7
RubaALmohya <ruba12399@gmail.com>
2022-05-18 13:56:33 +0300

    solved this lab

---
+++ Recursion_Lambda.py
@@ -0,0 +1,32 @@
+# Python3 program to count vowels
+# in a string
+ 
+# Function to check the Vowel
+import re
+
+
+def isVowel(check):
+    return check.upper()  in ['A', 'E', 'I', 'O', 'U'] 
+
+def isVowel2(check2):
+    return check2.lower() in ['a','e','i','o','u']
+
+# Returns count of vowels in str
+def countVowels(str):
+    count = 0
+    for i in range(len(str)):
+ 
+        # Check for vowel
+        if isVowel(str[i]):
+         
+            count += 1
+        elif isVowel2(str[i]):
+            count += 1
+
+    return count
+
+str = input("write :")
+print(countVowels(str))
+print("____________Q2_____________")
+result=list(map(lambda n: n * n, [40, 35, 10, 15, 20]))
+print(result)
