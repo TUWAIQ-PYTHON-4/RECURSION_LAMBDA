82a21d5693fd2399e4fd6c0cfeb951a1cc803fce
Abdulhadi <a-ahmasani@hotmail.com>
2022-05-18 14:40:04 +0300

    update the lab2

--- lab2.py
+++ lab2.py
@@ -1,25 +1,23 @@
 #recursion, if given a word/phrase return how many vowels(a,e,i,o,u)
-from itertools import count
-
-
-def isVowel(ch):
+def isConsonant(ch):
 	return ch.upper() in ['A', 'E', 'I', 'O', 'U']
 
-def countVowel(str):
-    count=0
-    for i in range(len(str)):
-        if isVowel(str[i]):
-            count +=1
-    return count 
+
+def totalConsonants(string, n):
+	
+	if n == 1:
+		return isConsonant(string[0])
+
+	return totalConsonants(string, n - 1) + isConsonant(string[n-1])
 
 
-str='I love python'
-print(countVowel(str))
+string = "I love python"
+print(totalConsonants(string, len(string)))
 
 #Q2
 old_list = [40,35, 10, 15, 20]
 
 new_list = old_list
 print("new list",new_list)
-result = list(map(lambda x, y: x + y, old_list, new_list))
+result = list(map(lambda x, y: x * y, old_list, new_list))
 print(result)
