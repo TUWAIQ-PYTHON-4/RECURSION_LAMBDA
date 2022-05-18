15ca9b29452b7db1617e2509b2884a987227fa79
ahmed <a-5amd@hotmail.com>
2022-05-18 16:04:56 +0300

    lambda labs

--- lambda.py
+++ lambda.py
@@ -1,22 +1,16 @@
-"""
-letter={"a","e","o","i","u"}
+def Check_Vow(ph:str):
+    low_ph=ph.lower()
+    v_letters="aeiou"
 
-def letters (inpur_word:str):
-    global letter
-    inpur_word={}
-    if len(inpur_word) == len(letter):
-     return len(letter)
-        
-
-letters("ahmed")
-
-def Check_Vow(string):
-    vowels= {"a","e","o","i","u"}
-    final = [each for each in string if each in vowels]
-    print(len(final))
+    if low_ph=="":
     
+      return(0)
 
-Check_Vow("ahmed")"""
+    if low_ph [0] in v_letters:
+          return 1+ Check_Vow(low_ph[1:])
+    return Check_Vow(low_ph[1:])
+    
+print(Check_Vow("ahmed"))
 numbers =[40,35, 10, 15, 20]
 
 nmumbermulti = list(map(lambda number : number*number,numbers))
