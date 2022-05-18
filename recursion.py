c20031cdf133d15036d1449eb01db6d443d22295
Asma Abufayah <aaha44@gmail.com>
2022-05-18 13:25:32 +0300

    lab solved

---
+++ recursion.py
@@ -0,0 +1,32 @@
+from unittest import result
+
+
+def isVowel(character): 
+  character = character.lower() 
+
+  vowels = "aeiou" 
+
+  if character in vowels : 
+    return 1
+  else:
+      return 0
+
+def countVowels(string, n): 
+	
+  if n == 1 :
+      return isVowel(string[0]) 
+  else:
+
+    return countVowels(string, n - 1) + isVowel(string[n - 1]) 
+
+ 
+string = "I love python"
+print(countVowels(string, len(string))) 
+
+
+
+newlst=[40,35, 10, 15, 20]
+
+filtered_numbers=list(map(lambda number:number*number,newlst))
+
+print(filtered_numbers)
